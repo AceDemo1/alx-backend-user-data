779db36f438c4cfaa2a9bebfe8de4c09b962421a
AceDemo1 <lawal641@gmail.com>
2024-11-20 15:26:42 +0000

    hashed

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -7,3 +7,7 @@ def hash_password(password: str) -> bytes:
     """hash password"""
     salt = bcrypt.gensalt()
     return bcrypt.hashpw(password.encode('utf-8'), salt)
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """is valid"""
+    return bcrypt.checkpw(password, hashed)
