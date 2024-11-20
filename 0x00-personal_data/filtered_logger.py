d7a81cd71848700257cbe626b4d0b5373d13b967
AceDemo1 <lawal641@gmail.com>
2024-11-20 09:35:24 +0000

    db

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,59 +1,105 @@
 #!/usr/bin/env python3
-"""returns the log message obfuscated"""
+"""
+Script for handling Personal Data
+"""
+
+from typing import List
 import re
 import logging
-import os
-from typing import List
-from mysql.connector import MySQLConnection
+from os import environ
+import mysql.connector
 
-PII_FIELDS = ('name', 'email', 'phone', 'ssn', 'password')
+
+# # PII fields to be redacted
+PII_FIELDS = ("name", "email", "phone", "ssn", "password")
+
+
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
+    """
+    Replaces sensitive information in a message with a redacted value
+    based on the list of fields to redact
+
+    Args:
+        fields: list of fields to redact
+        redaction: the value to use for redaction
+        message: the string message to filter
+        separator: the separator to use between fields
+
+    Returns:
+        The filtered string message with redacted values
+    """
+    for f in fields:
+        message = re.sub(f'{f}=.*?{separator}',
+                         f'{f}={redaction}{separator}', message)
+    return message
+
+
+def get_logger() -> logging.Logger:
+    """
+    Returns a Logger object for handling Personal Data
+
+    Returns:
+        A Logger object with INFO log level and RedactingFormatter
+        formatter for filtering PII fields
+    """
+    logger = logging.getLogger("user_data")
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(RedactingFormatter(list(PII_FIELDS)))
+    logger.addHandler(stream_handler)
+
+    return logger
+
+
+def get_db() -> mysql.connector.connection.MySQLConnection:
+    """
+    Returns a MySQLConnection object for accessing Personal Data database
+
+    Returns:
+        A MySQLConnection object using connection details from
+        environment variables
+    """
+    username = environ.get("PERSONAL_DATA_DB_USERNAME", "root")
+    password = environ.get("PERSONAL_DATA_DB_PASSWORD", "")
+    host = environ.get("PERSONAL_DATA_DB_HOST", "localhost")
+    db_name = environ.get("PERSONAL_DATA_DB_NAME")
+
+    cnx = mysql.connector.connection.MySQLConnection(user=username,
+                                                     password=password,
+                                                     host=host,
+                                                     database=db_name)
+    return cnx
 
 
 class RedactingFormatter(logging.Formatter):
-    """ Redacting Formatter class
-        """
+    """
+    Redacting Formatter class for filtering PII fields
+    """
 
     REDACTION = "***"
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
     def __init__(self, fields: List[str]):
+        """
+        Constructor method for RedactingFormatter class
+
+        Args:
+            fields: list of fields to redact in log messages
+        """
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
-        """format"""
-        record.msg = filter_datum(self.fields,
-                                  self.REDACTION, record.msg, self.SEPARATOR)
-        return super().format(record)
+        """
+        Formats the specified log record as text.
 
+        Filters values in incoming log records using filter_datum.
+        """
+        record.msg = filter_datum(self.fields, self.REDACTION,
+                                  record.getMessage(), self.SEPARATOR)
+        return super(RedactingFormatter, self).format(record)
 
-def get_logger() -> logging.Logger:
-    """logger"""
-    logger = logging.getLogger('user_data')
-    logger.setLevel(logging.INFO)
-    logger.propagate = False
-    stream = logging.StreamHandler()
-    fomatter = RedactingFormatter(PII_FIELDS)
-    stream.setFormatter(fomatter)
-    logger.addHandler(stream)
-    return logger
-
-
-def filter_datum(fields: List[str],
-                 redaction: str, message: str, separator: str) -> str:
-    """returns the log message obfuscated"""
-    for i in fields:
-        message = re.sub(f'{i}=.*?{separator}',
-                         f'{i}={redaction}{separator}', message)
-    return message
-
-
-def get_db() -> MySQLConnection:
-    """database"""
-    usr = os.getenv('PERSONAL_DATA_DB_USERNAME', 'root')
-    paswd = os.getenv('PERSONAL_DATA_DB_PASSWORD', '')
-    host = os.getenv('PERSONAL_DATA_DB_HOST', 'localhost')
-    db = os.getenv('PERSONAL_DATA_DB_NAME')
-    con = MySQLConnection(user=usr, password=paswd, host=host, database=db)
-    return con
