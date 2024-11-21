cebfe92a9142936a060f6a10d8c6a8a7e46aa886
AceDemo1 <lawal641@gmail.com>
2024-11-21 12:04:07 +0000

    basic auth

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""class to manage the API authentication"""
+import flask
+import request
+
+
+class Auth:
+    """define class"""
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """define class"""
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """define class"""
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """deine class"""
+        return None
