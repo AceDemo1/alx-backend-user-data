6d7faa6350e3be79f6b5a81456297aaf2eb42154
AceDemo1 <lawal641@gmail.com>
2024-11-21 14:34:58 +0000

    basic auth

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -9,6 +9,8 @@ class Auth:
     """define class"""
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """define class"""
+        if not path or not excluded_paths:
+            return True
         new_path = path if path[-1] == '/' else path + '/'
         new_excluded_paths = [i if i[-1] == '/' else i + '/' for i in excluded_paths]
         if new_path in new_excluded_paths:
