4325f350206eda1d3469c0fcf536c0ebd0dcefcc
AceDemo1 <lawal641@gmail.com>
2024-11-22 20:17:41 +0000

    basic auth

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -37,15 +37,17 @@ def forbidden(error) -> str:
     return jsonify({"error": "Forbidden"}), 403
 
 
+@app.before_request
 def before_request():
+    """before request"""
     if not auth:
         return None
     paths = ['/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/']
     if not auth.require_auth(request.path, paths):
         return
-    if not auth.authorization_header(request):
+    if auth.authorization_header(request) is None:
         abort(401)
-    if not auth.current_user(request):
+    if auth.current_user(request) is None:
         abort(403)
 
 if __name__ == "__main__":
